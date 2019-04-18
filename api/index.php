b33594bfe0f33cf34394d89f59bcfb9b0681463a
Don Gordon <don@rippletraining.com>
2019-04-18 02:34:48 -0700

    Added getPrizes endpoint

--- api/index.php
+++ api/index.php
@@ -40,6 +40,58 @@ class GW2LottoryController {
     }
 
     /**
+     * Get prizes from guild stash
+     * 
+     * @url GET /getPrizes/$tab
+     * 
+     * @param int $tab This is the index of the guild bank tabs
+     *            0 = Guild Stash
+     *            1 = Treasure Trove
+     *            2 = Deep Cave
+     * 
+     */
+    public function getPrizes( $tab = 0 ) {
+        // Settings for the prizes
+        $guild = $this->config['guilds'][0];
+
+        // Query the API for the guild stash info based on $tab
+        $stash = $this->api->guild()->stashOf( $guild['api_key'], $guild['guild_id'] )->get()[$tab];
+
+        // Grab all the item ids
+        $lookupIDs = array();
+        foreach( $stash->inventory as $item ){
+            if( is_object( $item ) ){
+                $lookupIDs[$item->id] = $item->id;
+            }
+        }
+
+        // fetch item info
+        $itemInfo = $this->api->items()->many( $lookupIDs );
+        
+        $prizeArray = array();
+        foreach( $stash->inventory as $item ) {
+            if( is_null( $item ) )
+                continue;
+                
+            // Find the item in the "Cache" (DIRTY)
+            $apiItem = $this->_searchItems( $itemInfo, 'id', $item->id );
+            $prizeArray[] = [
+                "id"            => $item->id,
+                "count"         => $item->count,
+                "name"          => $apiItem->name,
+                //description is optional
+                "description"   => ( property_exists( $apiItem, 'description' ) ? $apiItem->description : "" ),
+                "type"          => $apiItem->type,
+                "rarity"        => $apiItem->rarity,
+                "chat_link"     => $apiItem->chat_link,
+                "icon"          => $apiItem->icon
+            ];
+        }
+
+        return $prizeArray;
+    }
+
+    /**
      * Test DB (REMOVE FOR PRODUCTION)
      * 
      * @url GET /test
@@ -47,6 +99,20 @@ class GW2LottoryController {
     public function test() {
         $result = $this->conn->query( "SELECT * FROM `{$this->config['db_table']}` ORDER BY `time` DESC" )->fetch_all( MYSQLI_ASSOC );
         return $result;
+    }
+
+    /**
+     * Search the array of item objects for $value in $index
+     * 
+     */
+    private function _searchItems($array, $index, $value)
+    {
+        foreach($array as $arrayInf) {
+            if($arrayInf->{$index} == $value) {
+                return $arrayInf;
+            }
+        }
+        return null;
     }
 }
 
